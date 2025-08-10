903276a40ba46edab3e4b50a7d950d31b10c7e1c
Fazil <fazilmm860@gmail.com>
2025-08-10 19:20:26 +0530

    deleted all files

--- src/api/reportService.js
+++
@@ -1,7 +0,0 @@
-// /src/api/reportService.js
-import api from './axiosConfig';
-
-export const getDashboardStats = () => api.get('/reports/dashboard');
-export const downloadReport = (reportData) => api.post('/reports/download', reportData, {
-  responseType: 'blob', // Important for file downloads
-});
--- src/api/userService.js
+++
@@ -1,7 +0,0 @@
-// /src/api/userService.js
-import api from './axiosConfig';
-
-export const getAllUsers = (params) => api.get('/users', { params });
-export const updateUser = (userId, userData) => api.put(`/users/${userId}`, userData);
-export const deleteUser = (userId) => api.delete(`/users/${userId}`);
-export const changePassword = (passwordData) => api.put('/users/changepassword', passwordData);
